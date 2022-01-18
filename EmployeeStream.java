51ea940975bfc4aaa2d4b173b51ad4dd3c23746e
devarakondajagadeesh <djagadeesh2011@gmail.com>
2022-01-18 11:50:14 +0530

    first commit

---
+++ EmployeeStream.java
@@ -0,0 +1,11 @@
+package RealTimeQuery;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class EmployeeStream {
+	
+	List<Employee> employeelist = new ArrayList<Employee>();
+	employeelist.add(new Employee(121,"jagadeesh",24,"Male","partsdevelopment",2022,25000.0));
+
+}
